041da20a79c6d92d26f27dcc4c6f51a6c8b33adc
bijeesraj <bijeesraj007@gmx.com>
2018-10-29 21:12:46 -0500

    Linked List

---
+++ src/main/java/com/braj/interview/linkedlist/Main.java
@@ -0,0 +1,12 @@
+package com.braj.interview.linkedlist;
+
+public class Main {
+
+    public static void main(String[] args) {
+      LinkedList linkedList = new LinkedList();
+      linkedList.append(0);
+      linkedList.prepend(-1);
+
+        System.out.println(linkedList.toString());
+    }
+}
