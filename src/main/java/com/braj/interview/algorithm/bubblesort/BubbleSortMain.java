fa47b5289c8a0e27856c8d381ac2530b2bc74d2f
bijeesraj <bijeesraj007@gmx.com>
2018-10-30 20:16:35 -0500

    Bubble Sort

---
+++ src/main/java/com/braj/interview/algorithm/bubblesort/BubbleSortMain.java
@@ -0,0 +1,30 @@
+package com.braj.interview.algorithm.bubblesort;
+
+//https://www.youtube.com/watch?v=F13_wsHDIG4
+public class BubbleSortMain {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1,2,17,22,91,4,32,8,9,3,24,7};
+        System.out.println("Length : " + numbers.length);
+        numbers = bubbleSort(numbers);
+
+        for (int i = 0; i < numbers.length-1 ; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+
+    private static int[] bubbleSort(int[] numbers) {
+        int temp;
+        for(int i=0; i<numbers.length-1;i++){ // You will need to no n number of iteration
+            for(int j=0; j<numbers.length-1-i;j++){  // Work on nth and n+1 th element.
+                if(numbers[j] > numbers[j+1]) {
+                    temp = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = temp;
+                }
+            }
+        }
+        return numbers;
+    }
+}
